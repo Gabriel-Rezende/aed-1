6a9d337b93ce1a78f023d09e974e01ed0d2ba45d
giancarlomoraes <giancarlomoraes@hotmail.com>
2021-04-10 15:58:32 -0300

    Classes mapeadas e classe abstrata (Domain)

--- src/main/java/br/edu/ifgoiano/trindade/programacaoweb/projeto/test/Principal.java
+++ src/main/java/br/edu/ifgoiano/trindade/programacaoweb/projeto/test/Principal.java
@@ -9,28 +9,12 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
 
 import br.edu.ifgoiano.trindade.programacaoweb.projeto.domain.Empresa;
-import br.edu.ifgoiano.trindade.programacaoweb.projeto.domain.EmpresaRepository;
 import br.edu.ifgoiano.trindade.programacaoweb.projeto.domain.Cliente;
-import br.edu.ifgoiano.trindade.programacaoweb.projeto.domain.ClienteRepository;
 import br.edu.ifgoiano.trindade.programacaoweb.projeto.domain.Servico;
-import br.edu.ifgoiano.trindade.programacaoweb.projeto.domain.ServicoRepository;
 import br.edu.ifgoiano.trindade.programacaoweb.projeto.domain.ServicoCliente;
-import br.edu.ifgoiano.trindade.programacaoweb.projeto.domain.ServicoClienteRepository;
 
 @Component
 public class Principal implements CommandLineRunner {
-
-	@Autowired
-	private ServicoClienteRepository servicoClienteRepository;
-
-	@Autowired
-	private ClienteRepository clienteRepository;
-
-	@Autowired
-	private ServicoRepository servicoRepository;
-
-	@Autowired
-	private EmpresaRepository empresaRepository;
 
 	@Override
 	public void run(String... args) throws Exception {
