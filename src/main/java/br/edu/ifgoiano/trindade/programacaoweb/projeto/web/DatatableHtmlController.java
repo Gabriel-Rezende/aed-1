239bb88984f1ad33473cb0616a0f4eef97c3fa85
Gabriel Pires <gabrielpires763@gmail.com>
2021-05-26 12:14:27 -0300

    Cadastro de Serviços

--- src/main/java/br/edu/ifgoiano/trindade/programacaoweb/projeto/web/DatatableHtmlController.java
+++ src/main/java/br/edu/ifgoiano/trindade/programacaoweb/projeto/web/DatatableHtmlController.java
@@ -5,11 +5,9 @@ import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 
 import br.edu.ifgoiano.trindade.programacaoweb.projeto.domain.Empresa;
@@ -20,30 +18,44 @@ import br.edu.ifgoiano.trindade.programacaoweb.projeto.domain.ServicoRepository;
 @Controller
 @RequestMapping(path = "/servicos")
 public class DatatableHtmlController extends HtmlController<Servico, Integer> {
-    
-	@Autowired
-    private ServicoRepository servicoRepository;
-    
-    @Autowired
-    private EmpresaRepository empresaRepository;
 
-    @GetMapping("/empresa/{id}")
+	@Autowired
+	private ServicoRepository servicoRepository;
+
+	@Autowired
+	private EmpresaRepository empresaRepository;
+
+	@GetMapping("/empresa/{id}")
 	public ModelAndView listarServicosEmpresa(@PathVariable("id") Integer id) {
 		ModelAndView mv = new ModelAndView("/" + getNome());
 		List<Servico> servicos = servicoRepository.findByEmpresa(empresaRepository.findById(id));
 		mv.addObject("objetos", servicos);
 		mv.addObject("id", id);
-		
+
 		return mv;
 	}
-    
-    @Override
-    public String getNome() {
-        return "servicos";
-    }
-    
-    @Override
-    protected JpaRepository<Servico, Integer> getRepository() {
-        return servicoRepository;
-    }
+	
+	@RequestMapping("/add")
+	@Override
+	public ModelAndView add(Servico obj) {
+		ModelAndView mv = new ModelAndView("/" + getNome() + "Add");
+		mv.addObject("objeto", obj);
+		
+		Empresa empresa = new Empresa();
+		mv.addObject("e", empresa);
+		
+		List<Empresa> empresas = empresaRepository.findAll();
+		mv.addObject("empresas", empresas);
+		return mv;
+	}
+
+	@Override
+	public String getNome() {
+		return "servicos";
+	}
+
+	@Override
+	protected JpaRepository<Servico, Integer> getRepository() {
+		return servicoRepository;
+	}
 }
--- src/main/java/br/edu/ifgoiano/trindade/programacaoweb/projeto/web/HtmlController.java
+++ src/main/java/br/edu/ifgoiano/trindade/programacaoweb/projeto/web/HtmlController.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import javax.validation.Valid;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Sort;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Controller;
@@ -12,6 +13,8 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.ModelAndView;
+
+import br.edu.ifgoiano.trindade.programacaoweb.projeto.domain.EmpresaRepository;
 
 @Controller
 public abstract class HtmlController<T extends Object, ID extends Object> {
@@ -23,7 +26,7 @@ public abstract class HtmlController<T extends Object, ID extends Object> {
 	@GetMapping("")
 	public ModelAndView listar() {
 		ModelAndView mv = new ModelAndView("/" + getNome());
-		List<T> lista = getRepository().findAll(Sort.by(Sort.Direction.ASC, "id"));
+		List<T> lista = getRepository().findAll(Sort.by(Sort.Direction.ASC, "nome"));
 		mv.addObject("objetos", lista);
 		return mv;
 	}
